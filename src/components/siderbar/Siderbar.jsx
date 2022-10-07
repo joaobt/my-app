2234bd92249eeb3b9bb1f9d3908f96d2d3a91496
joao <joao4729@gmail.com>
2022-10-07 19:15:59 -0300

    finalizando trabalho

--- src/components/siderbar/Siderbar.jsx
+++ src/components/siderbar/Siderbar.jsx
@@ -1,25 +1,99 @@
-import './siderbar.css';
-import { LineStyle,Timeline, TrendingUp } from '@material-ui/icons';
-// import { Container } from './styles';
+import "./sidebar.css";
+import {
+  LineStyle,
+  Timeline,
+  TrendingUp,
+  PermIdentity,
+  Storefront,
+  AttachMoney,
+  BarChart,
+  MailOutline,
+  DynamicFeed,
+  ChatBubbleOutline,
+  WorkOutline,
+  Report,
+} from "@material-ui/icons";
+import { Link } from "react-router-dom";
 
-export default function Siderbar() {
+export default function Sidebar() {
   return (
-    <div className='siderbar'>
-      <div className='siderbarwrapper'>
-        <div className='sidebarMenu'>
-          <h3 className="siderbar">Dashboard</h3>
-          <ul className="siderbarList">
-            <li className="siderbarListItem">
-              <LineStyle />
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <Link to="/" className="link">
+            <li className="sidebarListItem active">
+              <LineStyle className="sidebarIcon" />
               Home
             </li>
-            <li className="siderbarListItem">
-              <Timeline />
+            </Link>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
               Analytics
             </li>
-            <li className="siderbarListItem">
-              <TrendingUp />
+            <li className="sidebarListItem">
+              <TrendingUp className="sidebarIcon" />
               Sales
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <Link to="/users" className="link">
+              <li className="sidebarListItem">
+                <PermIdentity className="sidebarIcon" />
+                Users
+              </li>
+            </Link>
+            <Link to="/products" className="link">
+              <li className="sidebarListItem">
+                <Storefront className="sidebarIcon" />
+                Products
+              </li>
+            </Link>
+            <li className="sidebarListItem">
+              <AttachMoney className="sidebarIcon" />
+              Transactions
+            </li>
+            <li className="sidebarListItem">
+              <BarChart className="sidebarIcon" />
+              Reports
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <MailOutline className="sidebarIcon" />
+              Mail
+            </li>
+            <li className="sidebarListItem">
+              <DynamicFeed className="sidebarIcon" />
+              Feedback
+            </li>
+            <li className="sidebarListItem">
+              <ChatBubbleOutline className="sidebarIcon" />
+              Messages
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <WorkOutline className="sidebarIcon" />
+              Manage
+            </li>
+            <li className="sidebarListItem">
+              <Timeline className="sidebarIcon" />
+              Analytics
+            </li>
+            <li className="sidebarListItem">
+              <Report className="sidebarIcon" />
+              Reports
             </li>
           </ul>
         </div>
@@ -27,6 +101,5 @@ export default function Siderbar() {
     </div>
   );
 }
-
 
 
